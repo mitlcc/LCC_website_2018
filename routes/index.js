dca24fc2bf048e0e7608a0bf2a789e64cf051448
Romeo Flores <romflores95@gmail.com>
2018-03-26 13:41:17 -0400

    added contact route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+/* GET contact. */
+router.get('/contact', function(req, res, next){
+  res.render('contact', {title: 'Contact'})
 });
 
 module.exports = router;
